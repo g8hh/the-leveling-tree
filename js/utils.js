83fdb27bc4d108b5ad3b09890f11559220f37404
zhaolinxu <zhaolinxu@sina.cn>
2021-05-20 10:19:05 +0800

    汉化

--- js/utils.js
+++ js/utils.js
@@ -262,10 +262,10 @@ function exportSave() {
 }
 
 function importSave(imported=undefined, forced=false) {
-	if (imported===undefined) imported = prompt("Paste your save here")
+	if (imported===undefined) imported = prompt("在这里粘贴你的存档")
 	try {
 		tempPlr = Object.assign(getStartPlayer(), JSON.parse(atob(imported)))
-		if(tempPlr.versionType != modInfo.id && !forced && !confirm("This save appears to be for a different mod! Are you sure you want to import?")) // Wrong save (use "Forced" to force it to accept.)
+		if(tempPlr.versionType != modInfo.id && !forced && !confirm("此保存似乎是针对其他mod！ 您确定要导入吗？")) // Wrong save (use "Forced" to force it to accept.)
 			return
 		player = tempPlr;
 		player.versionType = modInfo.id
